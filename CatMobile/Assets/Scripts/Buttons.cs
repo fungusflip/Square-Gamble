4bc99bdc0e64cbf55885870c65163c12e4f93958
fungusflip <alfredo.momp@gmail.com>
2023-06-08 23:19:55 +0200

    Some Scriptus

---
+++ CatMobile/Assets/Scripts/Buttons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    SerializeField gameObject prefab;
+    SerializeField transform SpawnPoint;
+
+    public void SpawnACat()
+    {
+        gameobject spawnacat = instantiate(prefab, )
+
+
+    }
+
+}
---
+++ CatMobile/Assets/Scripts/CatCheck.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBreed : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.activeSelf)
+        {
+            Debug.Log("It's Active");
+
+        }
+        
+    }
+
+    // Update is called once per frame
+}
