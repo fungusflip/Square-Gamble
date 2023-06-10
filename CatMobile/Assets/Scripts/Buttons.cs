a0592bdf461d084c1fd979014ecdf26f6acef6d3
fungusflip <alfredo.momp@gmail.com>
2023-06-10 18:27:32 +0200

    I want to die

--- CatMobile/Assets/Scripts/Buttons.cs
+++ CatMobile/Assets/Scripts/Buttons.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
-    SerializeField gameObject prefab;
-    SerializeField transform SpawnPoint;
+    [SerializeField] private GameObject prefab; // Right now what cat spawns
+    [SerializeField] private Transform spawnPoint; // Position for CatSpawning
+    [SerializeField] private GameObject breedMenu;  // BreedMenu
+    [SerializeField] private List<GameObject> commonCats;
+    [SerializeField] private List<GameObject> rareCats;
+    [SerializeField] private List<GameObject> legendaryCats;
+
+
+    public int catIntRed = 0;
+    public int catIntBlue = 0;
+    
+    
+
 
     public void SpawnACat()
     {
-        gameobject spawnacat = instantiate(prefab, )
+       float randomChance = Random.Range(0f, 1f);
 
+       
+       if (randomChance > 0.5f)
+       {
+         GameObject spawnedCat = Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+         catIntRed++;
+         Debug.Log(catIntRed);
+        
+         }
+        
+        if (randomChance > 0.7)
+        {
+
+
+        }
+    }
+       
+
+    public void BreedButtonActive()
+    {
+        
+        breedMenu.SetActive(true);
 
     }
-
 }
