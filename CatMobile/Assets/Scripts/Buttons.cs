710f043ca1ff5a4d685820454a5298f5ba9e3dbb
fungusflip <alfredo.momp@gmail.com>
2023-06-18 07:00:41 +0200

    Yuh
    Yuh

--- CatMobile/Assets/Scripts/Buttons.cs
+++ CatMobile/Assets/Scripts/Buttons.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
-    [SerializeField] private GameObject prefab; // Right now what cat spawns
+    [SerializeField] private GameObject prefabRed; // Right now what cat spawns
     [SerializeField] private Transform spawnPoint; // Position for CatSpawning
     [SerializeField] private GameObject breedMenu;  // BreedMenu
     [SerializeField] private List<GameObject> commonCats;
@@ -12,8 +12,12 @@ public class Buttons : MonoBehaviour
     [SerializeField] private List<GameObject> legendaryCats;
 
 
-    public int catIntRed = 0;
-    public int catIntBlue = 0;
+    public int catIntCommon = 0;
+    public int catIntRare = 0;
+    public int catIntLegendary = 0;
+    // Count variable
+    // random variable with count int
+    // Index with count int 
     
     
 
@@ -21,23 +25,42 @@ public class Buttons : MonoBehaviour
     public void SpawnACat()
     {
        float randomChance = Random.Range(0f, 1f);
+       int commonCatsNumber = commonCats.Count;
+       int rareCatsNumber = rareCats.Count;
+       int legendaryCatsNumber = legendaryCats.Count;
+       int commonCatsRandom = Random.Range(0, commonCatsNumber);
+       int rareCatsRandom = Random.Range(0, rareCatsNumber);
+       int legendaryCatsRandom = Random.Range(0, legendaryCatsNumber);
+
+       GameObject objectRandomCommon = commonCats[commonCatsRandom];
+       GameObject objectRandomRare = rareCats[rareCatsRandom];
+       GameObject objectRandomlegendary = legendaryCats[legendaryCatsRandom];
 
        
-       if (randomChance > 0.5f)
+       if (randomChance > 0.0f && randomChance < 0.5f)
        {
-         GameObject spawnedCat = Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
-         catIntRed++;
-         Debug.Log(catIntRed);
+         GameObject spawnedCat = Instantiate(objectRandomCommon, spawnPoint.position, spawnPoint.rotation);
+         catIntCommon++;
+         Debug.Log(catIntCommon);
         
-         }
-        
-        if (randomChance > 0.7)
-        {
-
-
         }
-    }
+
+        
+        if (randomChance > 0.5f && randomChance < 0.8f)
+      {
+        GameObject spawnedCat = Instantiate(objectRandomRare, spawnPoint.position, spawnPoint.rotation);
+        catIntRare++;
+        Debug.Log(catIntRare);
+      }
+        
+        if (randomChance > 0.8f && randomChance < 0.9f)
+      {
+        GameObject spawnedCat = Instantiate(objectRandomlegendary, spawnPoint.position, spawnPoint.rotation);
+        catIntLegendary++;
+        Debug.Log(catIntLegendary);
+      }
        
+    }
 
     public void BreedButtonActive()
     {
