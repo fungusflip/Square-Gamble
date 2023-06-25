a316df64920fcd359975e18e64fb0f2ef9bc0ec9
Lisa <lisa@malmqvist.com>
2023-06-25 19:06:36 +0200

    PP
    PP

--- CatMobile/Assets/Scripts/Movement.cs
+++ CatMobile/Assets/Scripts/Movement.cs
@@ -1,36 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rg;
-    [SerializeField] Transform transform;
-    [SerializeField] PositionConstraint positionConstraint;
+    [SerializeField] Transform myTransform;
+    Camera mainCamera;
+    Vector2 screenBounds;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Resolution currentResoultion = Screen.currentResoultion;
         rg = GetComponent<Rigidbody2D>();
-        positionConstraint.translationOffset = Screen.currentResoultion;
-
+        mainCamera = Camera.main;
+        screenBounds = GetScreenBounds();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        int TickCheckWalk = Random.Range(0, 10000);
-        float RandomX = Random.Range(-50f, 50f);
-        float RandomY = Random.Range(-50f, 50f);
+        float clampedX = Mathf.Clamp(myTransform.position.x, -screenBounds.x, screenBounds.x);
+        float clampedY = Mathf.Clamp(myTransform.position.y, -screenBounds.y, screenBounds.y);
+        Vector2 clampedPosition = new Vector2(clampedX, clampedY);
+        myTransform.position = clampedPosition;
 
-        if (TickCheckWalk == 1){
+        int tickCheckWalk = Random.Range(0, 10000);
+        float randomX = Random.Range(-100f, 100f);
+        float randomY = Random.Range(-100f, 100f);
 
-            Vector2 randomVector = new Vector2(RandomX*Time.deltaTime, RandomY*Time.deltaTime);
+        if (tickCheckWalk == 1)
+        {
+            Vector2 randomVector = new Vector2(randomX * Time.deltaTime, randomY * Time.deltaTime);
             rg.velocity = randomVector;
         }
+    }
 
-        
+    Vector2 GetScreenBounds()
+    {
+        float orthographicHeight = mainCamera.orthographicSize;
+        float orthographicWidth = orthographicHeight * mainCamera.aspect;
+
+        float maxScreenX = orthographicWidth - myTransform.localScale.x / 2f;
+        float maxScreenY = orthographicHeight - myTransform.localScale.y / 2f;
+
+        return new Vector2(maxScreenX, maxScreenY);
     }
 }
