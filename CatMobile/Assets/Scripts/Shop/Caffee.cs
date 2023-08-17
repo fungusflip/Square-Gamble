e8aadf387d2c6f6c92e6ec5fe85a6ccb2d10991f
fungusflip <alfredo.momp@gmail.com>
2023-08-17 22:28:18 +0200

    uu

---
+++ CatMobile/Assets/Scripts/Shop/Caffee.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Caffee: MonoBehaviour
+{
+	[SerializeField] HouseManager HouseManager;
+	[SerializeField] CashMoney cashmoney;
+	private GameObject house;
+
+    //cool Collider moment
+    //private void OnTriggerStay2D(Collider2D other)
+    /*{
+	   Drag otherDrag = other.GetComponent<Drag>();
+	   Quirk otherQuirk= other.GetComponent<Quirk>();
+	   if (otherDrag.dropped == true && other.gameObject.CompareTag("Cat"))
+	   {
+		   other.gameObject.SetActive(false);
+		   if(otherQuirk.evil == true)
+		   {
+			
+			cashmoney.goldIncrease += 5;
+		   }
+		   if(otherQuirk.evil == false)
+		   {
+			cashmoney.goldIncrease += 1;
+		   }
+	   }
+		  
+
+    }*/
+}
