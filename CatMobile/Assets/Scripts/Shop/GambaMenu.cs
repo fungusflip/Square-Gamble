ad00286ada82cbd387db1b18397435d4d3d8ea00
fungusflip <filip.renemark@student.forsbergsskola.se>
2023-10-07 16:42:48 +0200

    f
    f

--- CatMobile/Assets/Scripts/Shop/GambaMenu.cs
+++ CatMobile/Assets/Scripts/Shop/GambaMenu.cs
@@ -11,7 +11,7 @@ public class GambaMenu : MonoBehaviour
 
     void Update()
     {
-        if(cashMoney.cash >= 1400)
+        if(cashMoney.cash >= 100)
         {
             button.interactable = true;
         }   
